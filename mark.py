b0ffc58d5dab3a14beec5b68de5678f27155111e
张正彦 <zhangzhengyan14@mails.tsinghua.edu.cn>
2016-12-17 17:02:14 +0800

    add quid

--- mark.py
+++ mark.py
@@ -26,9 +26,10 @@ option_list = {'0':'None', '1':'Trump', '2':'Hillary'}
 quList = [quId]
 qu_ = re.compile(r'<.*?>')
 qu_br = re.compile(r'<br>')
+qu_n = re.compile(r'\n')
 for quId in quList:
     question = client.question(int(quId))
-    #output.write('content\001author\001voteup_count\001thanks_count\001comment_count\001id\001created_time\001updated_time\001support\001who_win\n')
+    #output.write('quId\001content\001author\001voteup_count\001thanks_count\001comment_count\001id\001created_time\001updated_time\001support\001who_win\n')
 
     for answer in question.answers:
         id = answer.id
@@ -37,7 +38,7 @@ for quId in quList:
         author = answer.author.name
         created_time = time.localtime(question.created_time)
         created_time_str = time.strftime('%Y-%m-%d %H:%M:%S', created_time)
-        content = answer.content.strip()
+        content = answer.content
         thanks_count = answer.thanks_count
         updated_time = time.localtime(question.updated_time)
         updated_time_str = time.strftime('%Y-%m-%d %H:%M:%S', updated_time)
@@ -45,19 +46,18 @@ for quId in quList:
         who_win = 0#
         p_content = qu_br.sub('\n',content)
         p_content = qu_.sub('',p_content)
-        content = qu_.sub('',content)
+        content = qu_.sub('',qu_n.sub('',content))
         print(p_content+'\n\n-----------------------------------------------------------------\n\n')
         while True:
-            support = raw_input('support whom:(0:none 1:trump 2:hi):')
-            who_win = raw_input('who win:(0:none 1:trump 2:hi):')
+            support = raw_input('support whom:(0:none 1:Trump 2:Hillary):')
+            who_win = raw_input('who win:(0:none 1:Trump 2:Hillary):')
             if support in option_list.keys() and who_win in option_list.keys():
-                print('ok')
                 support = option_list[support]
                 who_win = option_list[who_win]
                 break
         nums = [voteup_count,thanks_count,comment_count,id]
         nums = [str(x) for x in nums]
-        contents = [content, author]
+        contents = [str(quId),content, author]
         contents.extend(nums)
         contents.append(created_time_str)
         contents.append(updated_time_str)
