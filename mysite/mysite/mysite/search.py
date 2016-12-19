180379cbc5318e31220ac5d3433efa7c82da6299
songshshshsh <644240545@qq.com>
2016-12-19 14:09:50 +0800

    usage: use splitWords.py to generate fenci.txt and cd mysite and runserver

--- mysite/mysite/mysite/search.py
+++
@@ -1,6 +0,0 @@
-<form action="form_action.asp" method="get">
-  搜索内容: <input type="text" name="searchText" />
-  <input type="submit" value="搜索" />
-</form>
-  <input type = "button" name = "Previous" value = "上一页">
-  <input type = "button" name = "Next" value = "下一页">
